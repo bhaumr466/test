a8d43314134e0abebaabb5b96f40e414dfad60d1
bhaumr466 <80049176+bhaumr466@users.noreply.github.com>
2021-05-12 20:31:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,32 +4,34 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "vpc" "My_VPC" {
-  #source = "./variable"
+data "aws_availability_zones" "available" {}
 
-  name = "simple-example"
+locals {
+  cluster_name = "eu-central-1-eks-dev"
+}
 
-  cidr = "10.0.0.0/16"
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
 
-  azs             = ["eu-west-1a", "eu-west-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.70.0"
 
-  enable_ipv6 = false
+  name                 = "eu-central-1-vpc-dev"
+  cidr                 = "20.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["20.0.1.0/24", "20.0.2.0/24", "20.0.3.0/24"]
+  public_subnets       = ["20.0.4.0/24", "20.0.5.0/24", "20.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_s3_endpoint   = true
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  public_subnet_tags = {
-    Name = "overridden-name-public"
-  }
 
   tags = {
     Owner       = "rahul"
     Environment = "dev"
-  }
-
-  vpc_tags = {
-    Name = "vpc-name"
   }
 }
