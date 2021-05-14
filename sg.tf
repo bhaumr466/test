39b1e300ec23ab886e018ba15c3e14dff63a03c6
bhaumr466 <80049176+bhaumr466@users.noreply.github.com>
2021-05-14 21:37:40 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,17 @@
+module "security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "~> 3.0"
+  name = default-jenkins
+  description = jenkins-group
+  ingress_with_cidr_blocks = 0.0.0.0
+  #vpc_id = var.vpc_id
+  #egress_cidr_blocks = var.egress_cidr_blocks
+  #egress_rules = var.egress_rules
+  #ingress_with_source_security_group_id = var.ingress_with_source_security_group_id
+  #ingress_with_self = var.ingress_with_self
+
+
+#   tags = {
+#     environment = var.env
+#   }
+}
