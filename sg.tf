4854ecf9c727cc4ceec06f6295c6530a5288cc39
bhaumr466 <80049176+bhaumr466@users.noreply.github.com>
2021-05-14 22:11:03 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -2,8 +2,8 @@ module "security_group" {
   source = "terraform-aws-modules/security-group/aws"
   version = "~> 3.0"
   name = jenkins-test
-  #description = jenkins-group
-  #ingress_with_cidr_blocks = ["0.0.0.0/0"]
+  description = jenkins-group
+  ingress_with_cidr_blocks = ["0.0.0.0/0"]
   vpc_id = module.vpc.vpc_id
   #egress_cidr_blocks = var.egress_cidr_blocks
   #egress_rules = var.egress_rules
