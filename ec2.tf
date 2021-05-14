04c4b96677f3c428fc5c438a07cc6f591dacf17c
bhaumr466 <80049176+bhaumr466@users.noreply.github.com>
2021-05-14 20:48:41 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+module "ec2" {
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = "2.16.0"
+  name                        = "jenkins-test"
+  instance_count              = 1
+  ami                         = "ami-0b850cf02cc00fdc8"
+  instance_type               = "t2.micro"
+  #vpc_security_group_ids      = module.vpc.default_security_group_id
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  key_name                    = "jenkintest"
+  #user_data                   = file("${path.root}/scripts/${var.user_data}")
+  iam_instance_profile        = "jenkins-profile"
+  disable_api_termination     = true
+
+  root_block_device = [
+    {
+      volume_type = gp2
+      volume_size = 50
+    },
+  ]
+
+
+  tags = {
+    created_by  = "Terraform"
+    #name        = "${var.env}_${var.app_name}_vpc"
+    #environment = var.env
+    #project     = var.app_name
+  }
+}
