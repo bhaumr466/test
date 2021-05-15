cdb8db028f8914edab40aeb5fdcf35ec82e08fd1
bhaumr466 <80049176+bhaumr466@users.noreply.github.com>
2021-05-15 22:58:32 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,16 @@
 module "jsg" {
-  source = "terraform-aws-modules/security-group/aws"
-  version = "~> 4.0"
+  #source = "terraform-aws-modules/security-group/aws"
+  source = "clouddrove/security-group/aws"
+  #version = "~> 4.0"
   name = "jenkins_sg"
   description = "jenkins inbound"
   vpc_id             = module.vpc.vpc_id
   #egress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules       = ["all-all"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules      = ["all-all"] 
+  #egress_rules       = ["all-all"]
+  #ingress_cidr_blocks = ["0.0.0.0/0"]
+  #ingress_rules      = ["all-all"]
+   allowed_ip    = [module.vpc.vpc_cidr_block, "0.0.0.0/0"]
+   allowed_ports = [22]
 }
 
 
@@ -18,9 +21,9 @@ module "ec2" {
   instance_count              = 1
   ami                         = "ami-0b850cf02cc00fdc8"
   instance_type               = "t2.micro"
-  vpc_security_group_ids      = [module.jsg.jsg_id]
+  vpc_security_group_ids      = [module.jsg.security_group_ids]
   #vpc_security_group_ids      = [module.vpc.default_security_group_id]
-  
+
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   key_name                    = "jenkins-test"
