38ffda6121c77095d8559a2a926a378113b2fced
bhaumr466 <80049176+bhaumr466@users.noreply.github.com>
2021-05-15 10:58:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,14 @@
+module "jenkins_security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "~> 3.0"
+  name = "jenkins_security_group"
+  description = "jenkins inbound"
+  vpc_id             = module.vpc.vpc_id
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules      = ["all-all"] 
+}
 module "ec2" {
   source                      = "terraform-aws-modules/ec2-instance/aws"
   version                     = "2.16.0"
