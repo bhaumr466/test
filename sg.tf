05680adf285ff622bc56aa79e7ad8c3d5f95fc4d
bhaumr466 <80049176+bhaumr466@users.noreply.github.com>
2021-05-15 10:09:39 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,17 +1,17 @@
-#module "security_group" {
-  #source = "terraform-aws-modules/security-group/aws"
-  ##version = "~> 3.0"
-  ##name = jenkins-test
-  #description = jenkins-group
-  #ingress_with_cidr_blocks = ["0.0.0.0/0"]
-  #vpc_id = module.vpc.vpc_id
-  #egress_cidr_blocks = var.egress_cidr_blocks
-  #egress_rules = var.egress_rules
-  #ingress_with_source_security_group_id = var.ingress_with_source_security_group_id
-  #ingress_with_self = var.ingress_with_self
-
-
-#   tags = {
-#     environment = var.env
-#   }
-#}
+module "jenkins_security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "~> 3.0"
+  name = "jenkins_security_group"
+  description = "jenkins inbound"
+  vpc_id             = module.vpc.vpc_id
+  egress_cidr_blocks = "0.0.0.0/0"
+  egress_rules       = [all-all]
+  ingress_with_cidr_blocks = [
+    { 
+      from_port   = "22"
+      to_port     = "80"
+      protocol    = "tcp"
+      description = "jenkins port"
+      cidr_blocks = "0.0.0.0/0"
+  }, ]
+}
