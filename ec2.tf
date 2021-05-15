36469fa8475be2468f07b0896142ba7c3aba617f
bhaumr466 <80049176+bhaumr466@users.noreply.github.com>
2021-05-15 10:36:56 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ module "ec2" {
   instance_count              = 1
   ami                         = "ami-0b850cf02cc00fdc8"
   instance_type               = "t2.micro"
-  security_group_name         = module.jenkins_security_group_id
+  vpc_security_group_ids      = module.vpc.jenkins_security_group_id
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   key_name                    = "jenkins-test"
