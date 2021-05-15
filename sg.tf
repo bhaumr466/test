9d107f6f11a22f12fd6f7c2c00bd3b0fd4beed0c
bhaumr466 <80049176+bhaumr466@users.noreply.github.com>
2021-05-15 10:22:44 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -6,6 +6,6 @@ module "jenkins_security_group" {
   vpc_id             = module.vpc.vpc_id
   egress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
-  ingress_with_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules      = ["all-all"] 
 }
